63fbebfd4c038d4f25d23a7770caf13390f1b64a
Minh Nguyen <nguyenhminhp@gmail.com>
2016-05-19 21:24:57 -0700

    added phone texting

--- server.js
+++ server.js
@@ -1,22 +1,13 @@
 var express = require('express'); 
 var server = express();
 var bodyParser = require('body-parser');
-var apiKey = 'AC5c491fd19a50a024decf807c472647b9';
-var authToken = 'a87e42e64af5d26c2f20e6e61a269a3a';
+var	apiKey = 'AC5c491fd19a50a024decf807c472647b9';
+var	authToken = 'a87e42e64af5d26c2f20e6e61a269a3a';
 // Your accountSid and authToken from twilio.com/user/account
 var accountSid = apiKey;
 var authToken = authToken;
 var client = require('twilio')(accountSid, authToken);
  
-// client.messages.create({
-//     body: "Hello buddy, isnt this cool?",
-//     to: "+15613190727",
-//     from: "+14255239592"
-// }, function(err, message) {
-// 		console.log(err)
-//     process.stdout.write(message.sid);
-// });
-
 server.set('view engine', 'ejs')
 server.use(bodyParser.urlencoded({extended:true}))
 server.use(bodyParser.json());
@@ -32,34 +23,22 @@ server.get('/', function(req, res, next){
 	res.status(200).render('dignity')
 })
 
-// // server.get('/', function(req, res, next){
-// //   myCache.get( "results", function( err, value ){
-// //     if( !err ){
-// //       if(value == undefined){
-// //         async.parallel({
-// //           select: dbController.select,
-// //           topBusinessInCountry: dbController.topBusinessInCountry
-// //         },
-// //         function(err, results) {
-// //           myCache.set( "results", results, function( err, success ){
-// //             if( !err && success ){
-// //               res.status(200).render('index', results)
-// //             }
-// //           });
-// //         });
-// //       }else{
-// //         res.status(200).render('index', value)
-// //       }
-// //     }
-// //   });
-// // })
-
-// // server.post('/businesses', function(req, res, next){
-// //   var number = req.body.insertNumber;
-// //   console.log(number);
-// //   dbController.insertBusiness(number);
-// //   res.redirect('/');
-// // })
+server.post('/sendInfo', function(req, res, next){
+	console.log(req.body);
+	// var message = "Congrats David. You are the website owner of chuyenhausu.com. -MT Productions. This will be actual text: \n"
+	var message = "Name: " + req.body.first_name + " " + req.body.last_name + ". \n";
+	message += "Address: " + req.body.address + ", " + req.body.city + ", " + req.body.state + ", " + req.body.zip_code + ". \n";
+	message += "Number: " + req.body.number + ".";
+	// client.messages.create({
+ //    body: message,
+ //    to: "+18324907333",
+ //    from: "+14255239592"
+	// }, function(err, message) {
+	// 	console.log(err)
+ //    process.stdout.write(message.sid);
+	// });
+	res.send("done");
+})
 
 server.listen((3000), function(req, res){
   console.log("listening on " + (3000))
